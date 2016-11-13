f65e3032b51411bb231d3a3f2aadbbe0e4646e00
William Smith <williampsmith@berkeley.edu>
2016-11-13 05:16:44 -0800

    tired

--- rndr/RndrApp/ExploreViewController.swift
+++ rndr/RndrApp/ExploreViewController.swift
@@ -9,27 +9,47 @@
 import UIKit
 import GoogleMaps
 
-class ExploreViewController: UIViewController {
+class ExploreViewController: UIViewController, CLLocationManagerDelegate {
     
-    override func loadView() {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // get current location from user
+        var locManager = CLLocationManager()
+        locManager.delegate = self
+        locManager.requestAlwaysAuthorization()
+        
+        var currentLocation : CLLocation!
+        
+        if( CLLocationManager.authorizationStatus() == CLAuthorizationStatus.authorizedWhenInUse ||
+            CLLocationManager.authorizationStatus() == CLAuthorizationStatus.authorized) {
+            
+            currentLocation = locManager.location
+        }
+        else {
+            currentLocation = CLLocation(latitude: -33.86, longitude: 151.20)
+        }
+        
         // Create a GMSCameraPosition that tells the map to display the
         // coordinate -33.86,151.20 at zoom level 6.
-        let camera = GMSCameraPosition.camera(withLatitude: -33.86, longitude: 151.20, zoom: 6.0)
+        let camera = GMSCameraPosition.camera(withLatitude: currentLocation.coordinate.latitude, longitude: currentLocation.coordinate.longitude, zoom: 8.0)
         let mapView = GMSMapView.map(withFrame: CGRect.zero, camera: camera)
         mapView.isMyLocationEnabled = true
         view = mapView
         
-        // Creates a marker in the center of the map.
-        let marker = GMSMarker()
-        marker.position = CLLocationCoordinate2D(latitude: -33.86, longitude: 151.20)
-        marker.title = "Sydney"
-        marker.snippet = "Australia"
-        marker.map = mapView
-    }
-    
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
+        // todo: query the database and get the location of posts
+        var markerPositions : [CLLocation] = [currentLocation!]
+        
+        for var position in markerPositions {
+            // Creates a marker in the center of the map.
+            let marker = GMSMarker()
+            marker.position = CLLocationCoordinate2D(latitude: position.coordinate.latitude, longitude: position.coordinate.longitude)
+            marker.title = "Sydney"
+            marker.snippet = "Australia"
+            marker.map = mapView
+            
+        }
+        
         // Do any additional setup after loading the view.
     }
 
--- rndr/RndrApp/RndrApp/DataManager.swift
+++ rndr/RndrApp/RndrApp/DataManager.swift
@@ -11,6 +11,8 @@ import UIKit
 class DataManager: NSObject {
     
     func createPost(post: Post) {
+        var jsonString : NSString = ""
+        
         // create json files
         var json : [String : AnyObject] = [
             "author" : post.author, "type" : post.type as AnyObject,
@@ -31,18 +33,70 @@ class DataManager: NSObject {
                 // use dictFromJSON
             }
             
-            let jsonString = NSString(data: jsonData, encoding: String.Encoding.utf8.rawValue) as! String
-            jsonString
+            jsonString = NSString(data: jsonData, encoding: String.Encoding.utf8.rawValue)!
             
             // do something with json string here
             
         } catch {
             print(error.localizedDescription)
         }
+        
+        
+        let headers = [
+            "content-type": "application/json",
+            "cache-control": "no-cache",
+            "postman-token": "4a15bf02-cfb9-de37-bfbd-c373ec34157d"
+        ]
+        
+        var request = NSMutableURLRequest(url: NSURL(string: "https://rndrapp.herokuapp.com/post")! as URL,
+                                          cachePolicy: .useProtocolCachePolicy,
+                                          timeoutInterval: 10.0)
+        request.httpMethod = "POST"
+        request.allHTTPHeaderFields = headers
+        
+        let session = URLSession.shared
+        let dataTask = session.dataTask(with: request as URLRequest, completionHandler: { (data, response, error) -> Void in
+            if (error != nil) {
+                print(error)
+            } else {
+                let httpResponse = response as? HTTPURLResponse
+                print(httpResponse)
+            }
+        })
+        
+        dataTask.resume()
     }
     
     func retieveNearbyPosts() {
         // todo: implement function to retrieve from db
         // get current location
+        let headers = [
+            "cache-control": "no-cache",
+            "postman-token": "56af9e0b-e820-e7c5-917b-027cd21ae5f8"
+        ]
+        
+        var request = NSMutableURLRequest(url: URL(string: "https://rndrapp.herokuapp.com/posts/:lat/:lon")!, cachePolicy: .useProtocolCachePolicy, timeoutInterval: 10.0)
+        request.httpMethod = "GET"
+        request.allHTTPHeaderFields = headers
+        
+        let session = URLSession.shared
+        let dataTask = session.dataTask(with: request as URLRequest, completionHandler: { (data, response, error) -> Void in
+            if (error != nil) {
+                print(error)
+            } else {
+                let httpResponse = response as? HTTPURLResponse
+                print(httpResponse)
+            }
+        })
+        
+        dataTask.resume()
     }
 }
+
+
+
+
+
+
+
+
